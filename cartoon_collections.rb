2635c5ca55dc879660834182d64e79aeaae18178
Radniel Padron <radnielpadron@gmail.com>
2020-03-08 04:52:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,10 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   # binding.pry
+  array.map do |var|
+      
+  end
+  
 end
 
 def long_planeteer_calls(array)# code an argument here
