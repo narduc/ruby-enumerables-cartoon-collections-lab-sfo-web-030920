135ca07a503ec85b950a8c9962bdcc766cc80dc7
Radniel Padron <radnielpadron@gmail.com>
2020-03-08 04:55:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,12 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-   
+   array.each do |var|
+    if var.length > 4
+      return true
+    end
+   end
+   return false
 end
 
 def find_the_cheese(array)# code an argument here
